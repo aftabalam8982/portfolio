ed83dd7aff867767dc441413e14ad778db0bbedd
Aftab Alam <aftabf8982@gmail.com>
2024-05-15 15:24:55 +0530

    fixed typo

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -2,47 +2,47 @@ import React from "react";
 import "./about.style.css";
 
 const renderSeparator = () => (
-  <div className="separator-container">
-    <div className="separator"></div>
+  <div className='separator-container'>
+    <div className='separator'></div>
   </div>
 );
 const renderAboutSideBar = () => (
-  <div className="about-side-wrapper">
-    <div className="about-contact">
-      <div className="info-container">
-        <i className="fas fa-envelope"></i>
-        <div>afxxxxx25@gmail.com</div>
+  <div className='about-side-wrapper'>
+    <div className='about-contact'>
+      <div className='info-container'>
+        <i className='fas fa-envelope'></i>
+        <div>afxxxxx98@gmail.com</div>
       </div>
-      <div className="info-container">
-        <i className="fas fa-phone"></i>
+      <div className='info-container'>
+        <i className='fas fa-phone'></i>
         <div>+91 93xx xxxx 43</div>
       </div>
-      <div className="info-container">
-        <i className="fas fa-map-marker-alt"></i>
+      <div className='info-container'>
+        <i className='fas fa-map-marker-alt'></i>
         <div>Bangalore, India</div>
       </div>
     </div>
-    <div className="about-education">
-      <div className="about-title">Education</div>
-      <div className="about-description">
+    <div className='about-education'>
+      <div className='about-title'>Education</div>
+      <div className='about-description'>
         <b>BACHELOR OF TECHNOLOGY</b>
         <br />
-        <div className="title-secondary">
+        <div className='title-secondary'>
           <i>Mechanical Engineering</i>
         </div>
         Lakshmi Narain College of Technology, Bhopal, India
       </div>
     </div>
-    <div className="about-hobbies">
-      <div className="about-title">Hobbies</div>
-      <div className="about-description">Reading Articles and Books</div>
-      <div className="about-description">Playing Cricket</div>
-      <div className="about-description">Teaching & Mentoring</div>
+    <div className='about-hobbies'>
+      <div className='about-title'>Hobbies</div>
+      <div className='about-description'>Reading Articles and Books</div>
+      <div className='about-description'>Playing Cricket</div>
+      <div className='about-description'>Teaching & Mentoring</div>
     </div>
-    <div className="about-summary">
-      <div className="about-title">Summary</div>
-      <div className="about-description">
-        A passionate developer, philomath, bringing 2+ years of experience in
+    <div className='about-summary'>
+      <div className='about-title'>Summary</div>
+      <div className='about-description'>
+        A passionate developer, philomath, bringing 3.5+ years of experience in
         multiple Web Technologies and fostering relationships with clients,
         partners & team members. Proficient with front-end web development,
         learner of backend web-technologies, aspiring to be a proficient
@@ -53,16 +53,16 @@ const renderAboutSideBar = () => (
 );
 
 const renderAboutMain = () => (
-  <div className="about-main-wrapper">
-    <div className="about-work-experience">
-      <div className="about-title">Work Experience</div>
-      <div className="experience-details">
-        <div className="job-designation"> Software Engineer</div>
-        <div className="organisation-name">Pep Systems Pvt Ltd</div>
-        <div className="job-duration">
-          <i>07/2023 - Present</i>
+  <div className='about-main-wrapper'>
+    <div className='about-work-experience'>
+      <div className='about-title'>Work Experience</div>
+      <div className='experience-details'>
+        <div className='job-designation'> Software Engineer</div>
+        <div className='organisation-name'>Classic Informatics Pvt Ltd</div>
+        <div className='job-duration'>
+          <i>03/2022 - 07/2023</i>
         </div>
-        <div className="work-descriptions">
+        <div className='work-descriptions'>
           <ul>
             <li>
               Added a standard component library to be utilized across multiple
@@ -94,13 +94,13 @@ const renderAboutMain = () => (
         </div>
       </div>
       {renderSeparator()}
-      <div className="experience-details">
-        <div className="job-designation">Software Engineer</div>
-        <div className="organisation-name">Classic Informatics Pvt Ltd</div>
-        <div className="job-duration">
-          <i>03/2022 - 07/2023</i>
+      <div className='experience-details'>
+        <div className='job-designation'>Software Engineer</div>
+        <div className='organisation-name'>Deltra Soft Technology Pvt Ltd</div>
+        <div className='job-duration'>
+          <i>12/2019 - 03/2022</i>
         </div>
-        <div className="work-descriptions">
+        <div className='work-descriptions'>
           <ul>
             <li>
               Contributed to multiple projects using React and Redux, enhancing
@@ -143,7 +143,7 @@ const renderAboutMain = () => (
 );
 const About = () => {
   return (
-    <section id="about" className="about-wrapper">
+    <section id='about' className='about-wrapper'>
       {renderAboutSideBar()}
       {renderAboutMain()}
     </section>
--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -4,51 +4,51 @@ import BioImage from "../SVGs/BioImage";
 
 const Home = () => {
   return (
-    <section id="home" className="home-wrapper">
-      <div className="bio-info">
-        <div className="name">Aftab Alam</div>
-        <div className="designation">Software Engineer</div>
-        <div className="organization">
+    <section id='home' className='home-wrapper'>
+      <div className='bio-info'>
+        <div className='name'>Aftab Alam</div>
+        <div className='designation'>Software Engineer</div>
+        <div className='organization'>
           <a
-            href="https://www.pepit.org/contact.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.pepit.org/contact.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            @Pep Systems
+            @Classic Informatics
           </a>
         </div>
-        <div className="socials">
-          <div className="social-link">
+        <div className='socials'>
+          <div className='social-link'>
             <a
-              href="https://www.linkedin.com/in/aftab-alam-07341017b/"
-              target="_blank"
-              rel="noreferrer"
+              href='https://www.linkedin.com/in/aftab-alam-07341017b/'
+              target='_blank'
+              rel='noreferrer'
             >
-              <i className="fab fa-linkedin-in"></i>
+              <i className='fab fa-linkedin-in'></i>
               LinkedIn
             </a>
           </div>
-          <div className="social-link">
+          <div className='social-link'>
             <a
-              href="https://github.com/aftabalam8982"
-              target="_blank"
-              rel="noreferrer"
+              href='https://github.com/aftabalam8982'
+              target='_blank'
+              rel='noreferrer'
             >
-              <i className="fab fa-github"></i>
+              <i className='fab fa-github'></i>
               Github
             </a>
           </div>
         </div>
         <a
-          href="https://www.linkedin.com/in/aftab-alam-07341017b/"
-          className="btn btn-primary"
-          target="_blank"
-          rel="noreferrer"
+          href='https://www.linkedin.com/in/aftab-alam-07341017b/'
+          className='btn btn-primary'
+          target='_blank'
+          rel='noreferrer'
         >
           Hire Me
         </a>
       </div>
-      <div className="bio-image">
+      <div className='bio-image'>
         <BioImage />
       </div>
     </section>
